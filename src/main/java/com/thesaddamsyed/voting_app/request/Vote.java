7d923f321af1a641eb6a6e699decb73adfd5419d
thesaddamsyed <thesaddamsyed@gmail.com>
2025-07-15 19:03:31 +0530

    Add Vote class to handle voting requests with pollId and optionIndex

---
+++ src/main/java/com/thesaddamsyed/voting_app/request/Vote.java
@@ -0,0 +1,11 @@
+package com.thesaddamsyed.voting_app.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Vote {
+    private Long pollId;
+    private int optionIndex;
+}
